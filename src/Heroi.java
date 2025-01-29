d522933d48ebc7a0214f4f95274a65451b0010d7
serjoladf <serjolasrg@gmail.com>
2025-01-29 15:53:49 -0300

    implementação e finalização

--- src/Heroi.java
+++ src/Heroi.java
@@ -1,3 +1,45 @@
 public class Heroi {
-    
+    String nome;
+    int idade;
+    String tipoHeroi;
+    String ataque;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getTipoHeroi() {
+        return tipoHeroi;
+    }
+
+    public void setTipoHeroi(String tipoHeroi) {
+        this.tipoHeroi = tipoHeroi;
+    }
+
+    public void atacar() {
+        if (tipoHeroi.equals("mago")) {
+            ataque = "magia";
+        } else if (tipoHeroi.equals("guerreiro")) {
+            ataque = "espada";
+        } else if (tipoHeroi.equals("monge")) {
+            ataque = "artes marciais";
+        } else if(tipoHeroi.equals("ninja")){
+            ataque = "shuriken";
+        }
+        System.out.println("--------------------------------------------");
+        System.out.printf("--> O %s atacou usando %s <--\n",tipoHeroi,ataque);
+        System.out.println("--------------------------------------------");
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        Heroi heroi = new Heroi();
+        boolean acesso = true;
+
+        while (acesso){
+            System.out.println("====MENU PRINCIPAL=====");
+            System.out.println("Digite o nome do tipo de Heroi:\nmago\nguerreiro\nmonge\nninja\nsair - para encerrar o sistema\n");
+            heroi.setTipoHeroi(sc.nextLine().toLowerCase());
+
+            if (heroi.getTipoHeroi().contains("mago") || heroi.getTipoHeroi().contains("guerreiro") || heroi.getTipoHeroi().contains("monge") || heroi.getTipoHeroi().contains("ninja")) {
+                heroi.atacar();
+            }
+            else if(heroi.getTipoHeroi().contains("sair")){
+                acesso = false;
+            } else{
+                System.err.println("Opção inválido");
+            }
+        }
+        sc.close();
     }
 }
